9e1b326fb49c99c8b20b81eaec62032e19ba3be7
Antoine <antoine.jean.2@ulaval.ca>
2024-11-20 16:33:45 -0500

    outline for results production

---
+++ training_and_evaluation_functions.py
@@ -0,0 +1,20 @@
+def get_datasets(recipe_categories=["drinks", "bakery"]):
+    return []
+
+def split_datasets(datasets, test_ratio = 0.1):
+    return datasets, datasets
+
+def add_dangerous_data(test_datasets):
+    return test_datasets
+
+def train_model(model, n_epochs = 1):
+    return model
+
+def eval_perplexity(trained_model, test_data, bad_data = False):
+    
+    
+    
+    return 0
+
+def save_scores(scores):
+    pass
